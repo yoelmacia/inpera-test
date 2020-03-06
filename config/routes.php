01777a000f182a8c757ac98a2e52a0fc2eff1656
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-03-06 18:08:53 +0100

    Add route
    Add get route on /post to get our response from the read
    handler.

--- config/routes.php
+++ config/routes.php
@@ -35,4 +35,5 @@ use Psr\Container\ContainerInterface;
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+    $app->get('/post[/]', Post\Handler\PostReadHandler::class, 'post.read');
 };
