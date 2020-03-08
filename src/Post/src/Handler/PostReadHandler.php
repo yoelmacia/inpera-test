11a62f26756c2d928c359528406ee587f04043d9
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-03-08 18:04:47 +0100

    Use the entity manager
    Add createQueryBuilder which provides a set of
    classes and methods that is able to programmatically
    build queries.
    Add the get method to get all records of the db
    through Paginator.

--- src/Post/src/Handler/PostReadHandler.php
+++ src/Post/src/Handler/PostReadHandler.php
@@ -7,11 +7,30 @@ namespace Post\Handler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Zend\Diactoros\Response\JsonResponse;
+use Post\Entity\Post;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 class PostReadHandler implements RequestHandlerInterface
 {
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Create and return a response
+        $query = $this->entityManager->getRepository("Post\Entity\Post")
+            ->createQueryBuilder("alias")
+            ->getQuery();
+
+        $paginator = new Paginator($query);
+
+        $records = $paginator->getQuery()->getResult(Query::HYDRATE_ARRAY);
+        
+        return new JsonResponse($records);
     }
 }
