782700c81827d54472d1cf65663b86efdfe07395
Yoel Macia Delgado <yoelmaciadelgado@gmail.com>
2020-03-08 18:00:18 +0100

    Add entity manager to the factory

--- src/Post/src/Handler/PostReadHandlerFactory.php
+++ src/Post/src/Handler/PostReadHandlerFactory.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Post\Handler;
 
+use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
 
 class PostReadHandlerFactory
 {
     public function __invoke(ContainerInterface $container) : PostReadHandler
     {
-        return new PostReadHandler();
+        $entityManager = $container->get(EntityManager::class);
+        return new PostReadHandler($entityManager);
     }
 }
